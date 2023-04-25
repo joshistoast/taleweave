7d529ccd54a7a3944ebee272125af2331b008faa
Josh Corbett <joshwcorbett@icloud.com>
2023-04-25 16:02:40 -0600

    feat: :sparkles: test login creds in dev

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 import { enhance } from '$app/forms'
-</script>
+import { dev } from '$app/environment'
 
+const testLogin = () => {
+  const username = document.querySelector('input[name="username"]') as HTMLInputElement
+  const password = document.querySelector('input[name="password"]') as HTMLInputElement
+
+  username.value = 'joshistoast'
+  password.value = 'Vacation1'
+}
+
+</script>
 
 <form method="POST" class="grid gap-4" use:enhance>
   <h1 class="text-4xl font-bold">Login</h1>
@@ -18,6 +27,13 @@ import { enhance } from '$app/forms'
   <button type="submit" class="bg-gray-800 p-1 hover:bg-gray-700">
     Login
   </button>
+
 </form>
+
+{#if dev}
+  <button on:click={testLogin}>
+    Test Login
+  </button>
+{/if}
 
 <p>Don't have an account yet? <a href="/auth/register">Create an account</a></p>
