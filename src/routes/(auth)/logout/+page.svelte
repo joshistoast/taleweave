98c36a8d343932298fcebc307d722db0db829837
Josh Corbett <joshwcorbett@icloud.com>
2023-04-28 12:41:54 -0600

    fix: :bug: user state not changing on logout until refresh

--- src/routes/(auth)/logout/+page.svelte
+++ src/routes/(auth)/logout/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 import { page } from '$app/stores'
-import { enhance } from '$app/forms'
+// import { enhance } from '$app/forms'
 $: user = $page.data.user
 </script>
 
-<form method="POST" class="grid gap-4" use:enhance>
+<form method="POST" class="grid gap-4">
   <h1 class="text-4xl font-bold">Log Out</h1>
 
   {#if !user?.userId}
@@ -12,12 +12,12 @@ $: user = $page.data.user
   {:else}
     <p>Are you sure you want to log out?</p>
 
-    <div class="grid lg:grid-cols-2 text-center gap-2">
+    <div class="grid gap-2 text-center lg:grid-cols-2">
       <a href="/" class="p-1 bg-gray-900 hover:bg-gray-800">
         No, Cancel
       </a>
 
-      <button type="submit" class="bg-rose-500/10 text-rose-500 hover:bg-rose-500/20 p-1">
+      <button type="submit" class="p-1 bg-rose-500/10 text-rose-500 hover:bg-rose-500/20">
         Yes, Log Out
       </button>
     </div>
