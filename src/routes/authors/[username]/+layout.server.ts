11e3df15d76e78ead6905a0d25006554328fcb4b
Josh Corbett <joshwcorbett@icloud.com>
2023-04-28 11:53:00 -0600

    fix: :bug: missing author page results in 500 error

---
+++ src/routes/authors/[username]/+layout.server.ts
@@ -0,0 +1,21 @@
+import type { LayoutServerLoad } from './$types'
+import db from '$lib/server/db'
+import {
+  error,
+} from '@sveltejs/kit'
+
+export const load: LayoutServerLoad = async ({ params }) => {
+
+  const { username } = params
+  const author = await db.authUser.findUnique({
+    where: { username },
+  })
+
+  if (!author) {
+    throw error(404, 'Author not found')
+  }
+
+  return {
+    author,
+  }
+}
