485192af9aff6e52e727378cf80df697c18c5e87
Josh Corbett <joshwcorbett@icloud.com>
2023-04-28 14:19:08 -0600

    feat: :lipstick: redesign logout ui

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -2,19 +2,28 @@
 import { page } from '$app/stores'
 
 $: ({ pathname } = $page.url)
+$: ({ user } = $page.data)
 
 type LoginNavItem = {
   label: string
   href: string
+  show?: () => boolean | boolean
 }
 const nav: LoginNavItem[] = [
   {
     label: 'Log In',
     href: '/login',
+    show: () => !user,
   },
   {
     label: 'Register',
     href: '/register',
+    show: () => !user,
+  },
+  {
+    label: 'Log Out',
+    href: '/logout',
+    show: () => !!user,
   }
 ]
 </script>
@@ -25,13 +34,15 @@ const nav: LoginNavItem[] = [
     <p class="text-sm">Log in or register to start writing.</p>
   </div>
   <nav class="flex items-center border-b border-gray-800">
-    {#each nav as { label, href }}
-      <a
-        href={href}
-        class="py-3 px-4 border-b text-sm hover:bg-gray-900 {pathname === href ? 'text-orange-300 border-orange-300' : 'border-transparent'}"
-      >
-        {label}
-      </a>
+    {#each nav as { label, href, show }}
+      {#if show === undefined || show()}
+        <a
+          href={href}
+          class="py-3 px-4 border-b text-sm hover:bg-gray-900 {pathname === href ? 'text-orange-300 border-orange-300' : 'border-transparent'}"
+        >
+          {label}
+        </a>
+      {/if}
     {/each}
   </nav>
 </div>
--- src/routes/(auth)/logout/+page.svelte
+++ src/routes/(auth)/logout/+page.svelte
@@ -4,23 +4,16 @@ import { page } from '$app/stores'
 $: user = $page.data.user
 </script>
 
-<form method="POST" class="grid gap-4">
-  <h1 class="text-4xl font-bold">Log Out</h1>
+<form method="POST" class="grid gap-4 p-4">
+  <p>Are you sure you want to log out?</p>
 
-  {#if !user?.userId}
-    <p>You're already logged out</p>
-  {:else}
-    <p>Are you sure you want to log out?</p>
+  <div class="grid gap-2 text-center lg:grid-cols-2">
+    <a href="/" class="px-3 py-2 bg-gray-900 rounded-md hover:bg-gray-800">
+      No, Cancel
+    </a>
 
-    <div class="grid gap-2 text-center lg:grid-cols-2">
-      <a href="/" class="p-1 bg-gray-900 hover:bg-gray-800">
-        No, Cancel
-      </a>
-
-      <button type="submit" class="p-1 bg-rose-500/10 text-rose-500 hover:bg-rose-500/20">
-        Yes, Log Out
-      </button>
-    </div>
-
-  {/if}
+    <button type="submit" class="px-3 py-2 rounded-md bg-rose-500/10 text-rose-500 hover:bg-rose-500/20">
+      Yes, Log Out
+    </button>
+  </div>
 </form>
