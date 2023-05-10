62fd46a172a07bd5f894a65e94c4df0235712d12
Josh Corbett <joshwcorbett@icloud.com>
2023-05-10 10:25:39 -0600

    feat(tags): optional dropdown, optional allow custom tags, remove tags, clear input on select, etc.

--- src/lib/components/TagsInput.svelte
+++ src/lib/components/TagsInput.svelte
@@ -4,11 +4,14 @@ import type { Tag } from '@prisma/client'
 import { clickOutside } from '$lib/utils'
 import { page } from '$app/stores'
 import { createEventDispatcher } from 'svelte'
+import type { MaybeTag } from '$lib/utils'
 
 const dispatch = createEventDispatcher()
 
 export let placeholder= 'Search tags...'
-export let selectedTags: Tag[] = []
+export let selectedTags: MaybeTag[] = []
+export let dropdown = false
+export let allowCustomTags = false
 
 let loading = false
 let search = ''
@@ -16,8 +19,8 @@ let tagResults: Tag[] = []
 let resultsOpen: boolean
 $: resultsOpen = tagResults.length > 0
 
-$: isTagSelected = (tag: Tag) => {
-  return selectedTags.some((t) => t.id === tag.id)
+$: isTagSelected = (tag: MaybeTag) => {
+  return selectedTags.some((t) => t.name === tag.name)
 }
 
 const clearSearch = () => {
@@ -33,23 +36,33 @@ const onInput = async () => {
   if (search.length >= 3) {
     loading = true
     await fetchTags()
-      .then((res) => (tagResults = res))
+      .then((res) => {
+        if (allowCustomTags) {
+          const topTag = { name: search }
+          const resContainsSearch = res.some((t: Tag) => t.name === search)
+          tagResults = resContainsSearch ? res : [topTag, ...res]
+        } else {
+          tagResults = res
+        }
+      })
       .catch((err) => console.error(err))
       .finally(() => (loading = false))
   } else {
     tagResults = []
   }
 }
-const toggleTag = (tag: Tag) => {
+const toggleTag = (tag: MaybeTag) => {
   isTagSelected(tag) ? removeTag(tag) : selectTag(tag)
   dispatch('change', selectedTags)
 }
-const selectTag = (tag: Tag) => {
+const selectTag = (tag: MaybeTag) => {
   selectedTags = [...selectedTags, tag]
+  clearSearch()
   dispatch('select', tag)
 }
-const removeTag = (tag: Tag) => {
-  selectedTags = selectedTags.filter((t) => t.id !== tag.id)
+const removeTag = (tag: MaybeTag) => {
+  selectedTags = selectedTags.filter((t) => t.name !== tag.name)
+  clearSearch()
   dispatch('remove', tag)
 }
 </script>
@@ -58,7 +71,7 @@ const removeTag = (tag: Tag) => {
   <div
     class="relative"
     use:clickOutside
-    on:clickOutside={() => (resultsOpen = false)}
+    on:clickOutside={() => (dropdown && (resultsOpen = false))}
   >
     <!-- tag search -->
     <div class="relative">
@@ -76,7 +89,7 @@ const removeTag = (tag: Tag) => {
           </div>
         {/if}
         {#if search?.length}
-          <button on:click={clearSearch} class="p-1 rounded-md hover:bg-white/10">
+          <button on:click|preventDefault={clearSearch} class="p-1 rounded-md hover:bg-white/10">
             <Icon icon="fluent:dismiss-12-filled" class="w-4 h-4" />
           </button>
         {/if}
@@ -84,17 +97,20 @@ const removeTag = (tag: Tag) => {
     </div>
     <!-- tag results -->
     <div class="
-      {resultsOpen ? 'visible translate-y-0 opacity-100' : 'invisible translate-y-2 opacity-0'}
-      absolute w-full p-2 bg-gray-800 rounded-md top-[calc(100%+0.25rem)] transition-all duration-100 ease-in-out
+      {resultsOpen ? 'block' : 'hidden'}
+      {dropdown ? 'absolute bg-gray-800 p-2 top-[calc(100%+0.35rem)] rounded-md' : 'pt-2'}
+      w-full
     ">
       {#each tagResults as tag}
         <button
-          class="flex items-center justify-between w-full p-2 rounded-md hover:bg-white/10"
-          on:click={() => toggleTag(tag)}
+          class="flex items-center justify-between w-full p-2 transition-all duration-100 ease-in-out rounded-md hover:bg-white/10"
+          on:click|preventDefault={() => toggleTag(tag)}
         >
           <div class="flex items-center gap-1 px-1">
             <span class="text-sm">{tag.name}</span>
-            <span class="text-xs text-white/50">{tag._count.posts} post{tag._count.posts !== 1 ? 's' : ''}</span>
+            {#if tag.__count?.posts}
+              <span class="text-xs text-white/50">{tag._count.posts} post{tag._count.posts !== 1 ? 's' : ''}</span>
+            {/if}
           </div>
           <Icon icon={isTagSelected(tag) ? 'fluent:checkbox-checked-24-filled' : 'fluent:checkbox-unchecked-24-filled'} class="w-5 h-5" />
         </button>
@@ -103,9 +119,18 @@ const removeTag = (tag: Tag) => {
   </div>
   <!-- selected tags -->
   {#if selectedTags?.length}
-    <div>
+    <div class="flex flex-wrap gap-2 pt-2">
       {#each selectedTags as tag}
-        <span>{tag.name}</span>
+        <div class="flex items-center p-1 rounded-md bg-white/10">
+          <span class="px-1 text-sm">{tag.name}</span>
+          <button
+            title="Remove tag '{tag.name}'"
+            class="p-1 rounded-md hover:bg-white/10"
+            on:click|preventDefault={() => removeTag(tag)}
+          >
+            <Icon icon="fluent:dismiss-12-filled" class="w-4 h-4" />
+          </button>
+        </div>
       {/each}
     </div>
   {/if}
