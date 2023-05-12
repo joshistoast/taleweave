af4e30c252c8cf2180fc61f64e2c147dd21f0eaf
Josh Corbett <joshwcorbett@icloud.com>
2023-05-12 14:45:46 -0600

    feat(landing): :lipstick: animate cta icons on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,20 +11,20 @@ $: ({ recentlyFeatured, recentlyPublished } = data)
 
 <div class="grid gap-4">
 
-  <div class="grid gap-4 px-4 pt-16">
+  <div class="grid gap-4 px-4 pt-8">
     <div class="w-12 h-12 p-2 text-orange-300 bg-white/5 lg:p-3 lg:w-20 lg:h-20 rounded-xl">
       <Logo />
     </div>
     <h1 class="font-serif text-2xl font-bold lg:text-4xl">Welcome to Taleweave</h1>
     <p class="text-sm">Taleweave is an open-source, and easy to use platform for writers and authors alike to share their stories and works of literature freely. On this platform you as an author can create an account, write and edit your stories, and publish them for the world to see and appreciate. Taleweave is by writers, for writers.</p>
     <div class="flex flex-col gap-2 py-4 text-sm font-bold lg:flex-row lg:items-center">
-      <a class="flex items-center gap-2 px-3 py-2 text-orange-300 transition-all duration-100 ease-in-out rounded-md bg-orange-400/10 hover:bg-orange-400/20" href="/posts">
+      <a class="flex items-center gap-2 px-4 py-3 text-orange-300 transition-all duration-100 ease-in-out rounded-md group bg-orange-400/10 hover:bg-orange-400/20" href="/posts">
         <span>Start Reading</span>
-        <Icon icon="fluent:arrow-right-24-filled" />
+        <Icon icon="fluent:arrow-right-24-filled" class="w-5 h-5 transition-transform duration-200 group-hover:translate-x-1" />
       </a>
-      <a class="flex items-center gap-2 px-3 py-2 transition-all duration-100 ease-in-out rounded-md text-white/60 hover:text-gray-100 hover:bg-white/10" href="/posts/new">
+      <a class="flex items-center gap-2 px-4 py-3 transition-all duration-100 ease-in-out rounded-md group text-white/60 hover:text-gray-100 hover:bg-white/10" href="/posts/new">
         <span>Start Writing</span>
-        <Icon icon="fluent:edit-24-filled" />
+        <Icon icon="fluent:edit-24-filled" class="w-5 h-5 transition-transform duration-200 group-hover:rotate-12" />
       </a>
     </div>
   </div>
@@ -40,7 +40,7 @@ $: ({ recentlyFeatured, recentlyPublished } = data)
         <Icon icon="fluent:arrow-right-24-filled" class="w-4 h-4" />
       </a>
     </div>
-    <Feed posts={recentlyFeatured} />
+    <Feed showFeaturedFlags={false} posts={recentlyFeatured} />
   </div>
 
   <div class="grid gap-4 p-4 border-t border-white/10">
