4ee386875587570051fd8b693e058cdae33d5c53
Josh Corbett <joshwcorbett@icloud.com>
2023-05-19 13:34:48 -0600

    refactor(dx): :label: add types to selects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,7 @@
+import type { Prisma } from '@prisma/client'
 
 // Post counts
-export const countsOfPost = {
+export const countsOfPost: Prisma.PostSelect = {
   _count: {
     select: {
       bookmarks: true,
@@ -9,19 +10,24 @@ export const countsOfPost = {
 }
 
 // Select all relevant data for tags on a post
-export const tagsOfPostSelect = {
+export const tagsOfPostSelect: Prisma.PostSelect = {
   tags: {
     select: {
       id: true,
       name: true,
       createdAt: true,
       updatedAt: true,
+      _count: {
+        select: {
+          posts: true,
+        }
+      }
     }
   }
 }
 
 // Select all relevant author data
-export const authorOfPostSelect = {
+export const authorOfPostSelect: Prisma.PostSelect = {
   author: {
     select: {
       id: true,
@@ -32,31 +38,22 @@ export const authorOfPostSelect = {
 }
 
 // Selects all relevant data for post feed pages
-export const postOfFeedSelect = {
+export const postOfFeedSelect: Prisma.PostSelect = {
   id: true,
   createdAt: true,
   updatedAt: true,
   title: true,
   description: true,
   featured: true,
+  published: true,
   rating: true,
   ...authorOfPostSelect,
   ...countsOfPost,
   ...tagsOfPostSelect,
 }
 
-// Selects all relevant data for post pages
-export const postOfPageSelect = {
-  id: true,
-  createdAt: true,
-  updatedAt: true,
-  title: true,
-  description: true,
-  featured: true,
-  rating: true,
+// Selects all relevant data for a post's page (not feed)
+export const postOfPageSelect: Prisma.PostSelect = {
+  ...postOfFeedSelect,
   content: true,
-  published: true,
-  ...authorOfPostSelect,
-  ...countsOfPost,
-  ...tagsOfPostSelect,
 }
